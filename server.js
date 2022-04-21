1efcf35bceaabf00e2c65579da60210112eea01d
Kokwei1108 <54805628+Kokwei1108@users.noreply.github.com>
2022-04-21 09:06:18 +0800

    Setting tweak

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-var jsonfile = require('../project/sample2.json')
+var jsonfile = require('./json/sample2.json')
 
 app.get("/", (req,res) =>{
     console.log(`test`)
